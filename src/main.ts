365435cd9f0b06c7da81a9910eab4bd0dfaff944
Sinh Nguyen <sinh.nt1@urbox.vn>
2024-11-23 02:53:10 +0700

    fix: corsing

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,8 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
-      'https://develop-time-tracking-ute.vercel.app/',
-      'https://time-tracking-ute.vercel.app/',
+      'https://develop-time-tracking-ute.vercel.app',
+      'https://time-tracking-ute.vercel.app',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders:
