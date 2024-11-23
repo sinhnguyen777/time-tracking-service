33dd05390b8c7700c1176565b9396f46d3700fad
Sinh Nguyen <sinh.nt1@urbox.vn>
2024-11-23 07:11:11 +0700

    fix: expiresIn

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,5 +2,5 @@ import { JwtModuleOptions } from '@nestjs/jwt';
 
 export const jwtConfig: JwtModuleOptions = {
   secret: 'your_secret_key', // Đổi thành secret thực tế
-  signOptions: { expiresIn: '1h' }, // Token hết hạn sau 1 giờ
+  signOptions: { expiresIn: '7d' }, // Token hết hạn sau 7 ngày
 };
